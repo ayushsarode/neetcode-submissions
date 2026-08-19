09b085a35055621eee7a04e40a92d314f02769bc
Ayush Sarodey <72145248+ayushsarode@users.noreply.github.com>
2026-08-19 18:10:34 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.java
@@ -0,0 +1,22 @@
+// brute force method
+
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        //outer loop
+
+        for (int i =0;i < nums.length; i++) {
+            int count=  0;
+
+            for (int j= 0; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+            if (count > nums.length/2) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
