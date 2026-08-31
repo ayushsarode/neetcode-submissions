d7ae2c6b682c6ae5307e8fbb56695804e2512689
Ayush Sarodey <72145248+ayushsarode@users.noreply.github.com>
2026-08-31 13:02:30 +0530

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int n = numbers.length;
+
+        for(int i = 0; i < n -1; i++) {
+            int complement = target - numbers[i];
+
+            int left = i + 1;
+            int right = n - 1;
+            int firstTrueIndex = -1;
+
+            while(left <= right) {
+                int mid = left + (right - left) / 2;
+                if(numbers[mid] >= complement) {
+                    firstTrueIndex = mid;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            if(firstTrueIndex != -1 && numbers[firstTrueIndex] == complement) {
+                return new int[] {i + 1, firstTrueIndex + 1};
+            }
+        }
+        return new int[] {};
+    }
+}
